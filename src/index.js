8b6f9a90dd47a21667281a25090f3436bc99b09e
SMN947 <betum98@gmail.com>
2022-04-07 10:32:13 -0500

    2022-04-07

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+app = express();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    //eslint-disable-next-line
+    console.log(`App listening on port ${PORT}`)
+})
+try {
+
+} catch (error) {
+    jump()
+}
+
+
+//eslint-disable-next-line
+console.log(`App listening on port ${PORT}`)
+console.log(`App listening on port ${PORT}`)
